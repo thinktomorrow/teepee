ba251de57c3a43666da3de3861b86f6037c40175
kingjohnny <johnny@thinktomorrow.be>
2017-04-04 21:44:08 +0200

    Yarnit

--- README.md
+++ README.md
@@ -1,26 +1,14 @@
 <img src="assets/img/logo.png" alt="Teepee"/>
-# chief-teepee
 
 ## Dependencies:
 - Warpaint
 - Animate.css
 - Jquery
 - Font-awesome
-- Gulp
-- Laravel
 
 ## Run the application:
 1. Open Terminal
 2. Git pull for updates/changes
-3. bower install kingjohnny/chief-teepee
+3. yarn install kingjohnny/teepee
 4. Create your build file (Gruntfile.js and build.scss)
 5. Have a Pow wow
-
-
-## Resources
-### Sass
-The main file (usually labelled chief.scss) should be the only Sass file from the whole code base not to begin with an underscore. This file should not contain anything but @import and comments.
-
-### JS
-
-### Compiling with Gulp
--- index.php
+++ index.php
@@ -14,22 +14,25 @@
 	<meta name="keywords" content="[]" />
 	<meta name="robots" contents="[]" />
 
+	<link rel="shortcut icon" href="assets/img/favicon.ico" />
+
 	<!-- social meta -->
 	<?php include('views/partials/metaheader') ?>
 
 	<!-- styles -->
-	<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/normalize/2.1.0/normalize.css" />
-	<link rel="stylesheet" href="assets/css/index.css" />
+	<link rel="stylesheet" href="node_modules/warpaint/warpaint.css" />
+	<link rel="stylesheet" href="node_modules/font-awesome/css/font-awesome.css" />
+	<link rel="stylesheet" href="node_modules/animate/animate.css" />
+
+	<link rel="stylesheet" href="assets/css/teepee.css" />
 
 	<!-- icons -->
-	<link rel="shortcut icon" href="assets/img/favicon.ico" />
 	<link rel="apple-touch-icon-precomposed" href="assets/img/apple-touch-icon-precomposed.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/img/apple-touch-icon-72x72-precomposed.png" />
 	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/img/apple-touch-icon-114x114-precomposed.png" />
 
 	<!-- head scripts -->
-	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.2/jquery.min.js"></script>
-	<!--[if lt IE 9]><script src="//html5shim.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+	<script src="node_modules/jquery/dist/jquery.css"></script>
 
 </head>
 
