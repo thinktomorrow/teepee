20f90e833d857a81c9371d9a76f95cb1e4128c0a
Kingjohnny <johnny@thinktomorrow.be>
2020-04-19 08:54:09 +0200

    add small instructions

--- public/index.php
+++ public/index.php
@@ -1,4 +1,16 @@
 <!-- Routing en algemene variabelen -->
 <?php require('config.php'); ?>
 <!-- Hieronder halen we de master-file op -->
-<?php require('views/_layout/master.php'); ?>
+<?php
+$filename = 'views/_layout/master.php';
+if (file_exists($filename)) {
+    include($filename); 
+} else {
+    print "<br>
+    The file <mark>$filename</mark> does not exist yet.<br><br>
+    Try running
+    <pre><mark>|composer install<br>|npm install<br>|npm run dev/prod/watch</mark></pre><br>
+    That should do the trick.";
+    
+}
+?>
